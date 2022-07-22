ef317d5adcb9547936a8e1254a4729aee0a70153
OlgaDormio2 <dormioo67@gmail.com>
2022-07-22 17:17:25 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-// Import stylesheets
-import './style.css';
+// // Import stylesheets
+// import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// // Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let input = document.querySelector('input');
+let textarea = document.querySelector('textarea');
+// This event listener has been implemented to identify a
+// Change in the input section of the html code
+// It will be triggered when a file is chosen.
+input.addEventListener('change', () => {
+  let files = input.files;
+  if (files.length === 0) return;
+  /* If any further modifications have to be made on the
+Extracted text. The text can be accessed using the
+file variable. But since this is const, it is a read
+only variable, hence immutable. To make any changes,
+changing const to var, here and In the reader.onload
+function would be advisible */
+  const file = files[0];
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    const file = e.target.result;
+    // This is a regular expression to identify carriage
+    // Returns and line breaks
+    const lines = file.split(/ | /);
+    textarea.value = lines.join(' ');
+  };
+  reader.onerror = (e) => alert(e.target.error.name);
+  reader.readAsText(file);
+});
